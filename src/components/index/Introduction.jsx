61968531d7338d6dde6c2b6a6955955e7500bbff
naddang <difedal@gmail.com>
2024-12-05 01:40:08 +0900

    개인정보 보호용

--- src/components/index/Introduction.jsx
+++ src/components/index/Introduction.jsx
@@ -11,8 +11,8 @@ function Introduction() {
         <div className="intro-content">
           <Image src="/assets/images/common/profile.jpg" alt="프로필 이미지" className="profile-image" />
           <div className="intro-text">
-            <h1>백엔드 개발자 OOO입니다</h1>
-            <p>2년 6개월의 경력을 가지고 있습니다.</p>
+            <h1>OOO입니다</h1>
+            <p>XX의 경력을 가지고 있습니다.</p>
             <Link to="/about" className="cta-button">소개 자세히 보기</Link>
           </div>
         </div>
